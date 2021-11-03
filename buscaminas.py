a9b2979c43c5a831dbd006313993d85562fe66e1
EnzoPonce <48955378+EnzoPonce@users.noreply.github.com>
2021-11-03 12:30:01 -0300

    Add files via upload

---
+++ buscaminas.py
@@ -0,0 +1,73 @@
+import random
+import yaml
+
+
+class Buscaminas():
+    def __init__(self, rows=None, cols=None, bombs=None):
+        self.board = []
+        self.rows = rows
+        self.cols = cols
+        for i in range(0, rows):
+            v = [" "]*cols
+            self.board.append(v)
+        mi = 1
+        while mi <= bombs:
+            fil = random.randint(0, rows - 1)
+            col = random.randint(0, cols - 1)
+            if " " in self.board[fil][col]:
+                self.board[fil][col] = "B"
+                mi += 1
+        print(self.board)
+
+    def play(self):
+
+        pass
+
+    def show_board(self):
+        return print(yaml.dump(self.board))
+
+    def question(self, movs):
+        self.move = movs
+        movs = ['flag', 'uncover']
+
+        while True:
+            mov = input('Elegir Movimiento:\n1) Flag 2) Uncover\n')
+            if mov.lower() not in ('flag', 'uncover'):
+                raise Exception()
+            else:
+                break
+
+        while True:
+            row = input('\nIngrese la fila a cambiar: ')
+            if str(row) not in [str(x) for x in range(self.rows)]:
+                raise Exception()
+            else:
+                row = int(row)
+                break
+
+        while True:
+            col = input('\nIngrese la columna a cambiar: ')
+            if str(col) not in [str(x) for x in range(self.cols)]:
+                raise Exception()
+            else:
+                col = int(col)
+                break
+
+        return [mov, row, col]
+
+
+    def win(self):
+        count = 1
+        for x in range(len(self.board)):
+            if "F" in self.board[x]:
+                count += 1
+        if count != 9:
+            return True
+        return False
+
+
+    def lose(self):
+        for i in range(len(self.board)):
+            if i in self.board[i] == "B":
+                return False
+            return True
